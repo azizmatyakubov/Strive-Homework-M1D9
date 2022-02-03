b44603b7d47bb7dd166328350d4ced3c2677270a
AZIZ <a.matyaqubov1471@gmail.com>
2022-02-03 16:38:16 +0100

    create random number function

--- script.js
+++ script.js
@@ -18,3 +18,19 @@ const displayNumbers = () => {
     lastDiv.innerText = 'BINGO'
     container.appendChild(lastDiv)
 }
+
+const getRandomNumber = () => {
+    let randomNums = []
+    for(let i=0; i<24; i++) {
+        let randomNum = Math.floor(Math.random() * 77) 
+        randomNums.push(randomNum)
+        let randomNumberNode = document.createElement('div')
+        randomNumberNode.classList.add('randomNumber')
+        randomNumberNode.innerText = randomNums[i]
+        document.getElementById('randomNumber').appendChild(randomNumberNode)
+    }
+
+    console.log(randomNums)
+}
+
+getRandomNumber()
